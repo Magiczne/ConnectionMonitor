821fdf7344035ac449e9dafa844ce8fcfb4d593d
Magiczne <michalkamilkleszczynski@gmail.com>
2017-08-19 10:19:22 +0200

    TextWriter implementation

---
+++ ConnectionMonitor/Writer/TxtWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConnectionMonitor.Writer
+{
+    internal class TxtWriter : IDataWriter
+    {
+        private readonly string _path;
+
+        public TxtWriter()
+        {
+            _path = Path.Combine("Logs", $"{DateTime.Now:dd-MM-yyyy}", $"{DateTime.Now:hh-mm-ss}.txt");
+        }
+
+        public void WriteConnectionLost()
+        {
+            var message = $"[{DateTime.Now:d}] Connection Lost";
+            File.AppendAllText(_path, message);
+        }
+
+        public void WriteConnectionRestored()
+        {
+            var message = $"[{DateTime.Now:d}] Connection Restored";
+            File.AppendAllText(_path, message);
+        }
+    }
+}
