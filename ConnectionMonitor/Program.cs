c30fc252237d49ac8c0327f39de068d009df306b
Magiczne <michalkamilkleszczynski@gmail.com>
2017-08-19 10:44:13 +0200

    Main implementation

--- ConnectionMonitor/Program.cs
+++ ConnectionMonitor/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ConnectionMonitor.Writer;
 
 namespace ConnectionMonitor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var writer = new TxtWriter();
+            var monitor = new Monitor.ConnectionMonitor(writer);
+
+            Console.WriteLine("Connection Monitor");
+            Console.WriteLine("Starting monitoring at {0:g}", DateTime.Now);
+            Console.WriteLine("Writing to file: {0}", writer.Path);
+
+            monitor.Start();
         }
     }
 }
