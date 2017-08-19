4faf8cf6a528690cc8608480cc1a979a2d833083
Magiczne <michalkamilkleszczynski@gmail.com>
2017-08-19 10:57:35 +0200

    Some docs

--- ConnectionMonitor/Writer/TxtWriter.cs
+++ ConnectionMonitor/Writer/TxtWriter.cs
@@ -5,6 +5,9 @@ namespace ConnectionMonitor.Writer
 {
     internal class TxtWriter : IDataWriter
     {
+        /// <summary>
+        /// Path of the log file being used
+        /// </summary>
         public string FilePath { get; }
 
         public TxtWriter()
@@ -24,18 +27,27 @@ namespace ConnectionMonitor.Writer
             }
         }
 
+        /// <summary>
+        /// Writes connection lost information into the file
+        /// </summary>
         public void ConnectionLost()
         {
             var message = $"[{DateTime.Now:d}] Connection Lost";
             File.AppendAllText(FilePath, message);
         }
 
+        /// <summary>
+        /// Writes connection restored information into the file
+        /// </summary>
         public void ConnectionRestored()
         {
             var message = $"[{DateTime.Now:d}] Connection Restored";
             File.AppendAllText(FilePath, message);
         }
 
+        /// <summary>
+        /// Writed monitoring started information into the file
+        /// </summary>
         public void MonitoringStarted()
         {
             var message = $"[{DateTime.Now:d}] Monitoring started";
