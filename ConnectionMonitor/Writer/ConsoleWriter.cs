72daaa706898f93a070c34e8a6c46507563b3921
Magiczne <michalkamilkleszczynski@gmail.com>
2017-08-20 12:10:24 +0200

    Docs cleanup

--- ConnectionMonitor/Writer/ConsoleWriter.cs
+++ ConnectionMonitor/Writer/ConsoleWriter.cs
@@ -4,18 +4,27 @@ namespace ConnectionMonitor.Writer
 {
     internal class ConsoleWriter : IDataWriter
     {
+        /// <summary>
+        ///     Writes connection lost information into console
+        /// </summary>
         public void ConnectionLost()
         {
             Console.WriteLine();
             Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Connection Lost");
         }
 
+        /// <summary>
+        ///     Writes connection restored information into console
+        /// </summary>
         public void ConnectionRestored()
         {
             Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Connection Restored");
             Console.WriteLine();
         }
 
+        /// <summary>
+        ///     Writed monitoring started information into console
+        /// </summary>
         public void MonitoringStarted()
         {
             Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Monitoring started");
