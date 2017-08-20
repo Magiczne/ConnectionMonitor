ad3072acb8c4f0db95b9a6aba639c3e0a9e07eb6
Magiczne <michalkamilkleszczynski@gmail.com>
2017-08-20 12:08:39 +0200

    ConsoleWriter added

---
+++ ConnectionMonitor/Writer/ConsoleWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConnectionMonitor.Writer
+{
+    internal class ConsoleWriter : IDataWriter
+    {
+        public void ConnectionLost()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Connection Lost");
+        }
+
+        public void ConnectionRestored()
+        {
+            Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Connection Restored");
+            Console.WriteLine();
+        }
+
+        public void MonitoringStarted()
+        {
+            Console.WriteLine($"[{DateTime.Now:hh:mm:ss}] Monitoring started");
+        }
+    }
+}
